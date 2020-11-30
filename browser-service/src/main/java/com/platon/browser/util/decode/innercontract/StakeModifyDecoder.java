1b122ce7607f319f47a284e0a6f3ab58acb6f3ec
chendongming <chendongming@juzix.net>
2020-11-30 17:38:28 +0800

    解码质押修改参数

--- browser-service/src/main/java/com/platon/browser/util/decode/innercontract/StakeModifyDecoder.java
+++ browser-service/src/main/java/com/platon/browser/util/decode/innercontract/StakeModifyDecoder.java
@@ -2,6 +2,7 @@ package com.platon.browser.util.decode.innercontract;
 
 import com.platon.browser.param.StakeModifyParam;
 import com.platon.browser.param.TxParam;
+import org.apache.commons.lang3.StringUtils;
 import org.web3j.rlp.RlpList;
 import org.web3j.rlp.RlpString;
 import org.web3j.utils.Numeric;
@@ -22,32 +23,32 @@ public class StakeModifyDecoder {
     static TxParam decode(RlpList rootList) {
         // 修改质押信息
         //用于接受出块奖励和质押奖励的收益账户
-        String address = addressResolver((RlpString) rootList.getValues().get(1));
+        String benefitAddress = addressResolver((RlpString) rootList.getValues().get(1));
         //被质押的节点的NodeId
         String nodeId = stringResolver((RlpString) rootList.getValues().get(2));
         //外部Id
         BigInteger rewardPer = bigIntegerResolver((RlpString) rootList.getValues().get(3));
         //外部Id externalId
         String externalId = stringResolver((RlpString) rootList.getValues().get(4));
-        externalId = new String(Numeric.hexStringToByteArray(externalId));
+        externalId = externalId==null?null:new String(Numeric.hexStringToByteArray(externalId));
         //被质押节点的名称
         String nodeName = stringResolver((RlpString) rootList.getValues().get(5));
-        nodeName = new String(Numeric.hexStringToByteArray(nodeName));
+        nodeName = nodeName==null?null:new String(Numeric.hexStringToByteArray(nodeName));
         //节点的第三方主页
         String website = stringResolver((RlpString) rootList.getValues().get(6));
-        website = new String(Numeric.hexStringToByteArray(website));
+        website = website==null?null:new String(Numeric.hexStringToByteArray(website));
         //节点的描述
         String detail = stringResolver((RlpString) rootList.getValues().get(7));
-        detail = new String(Numeric.hexStringToByteArray(detail));
+        detail = detail==null?null:new String(Numeric.hexStringToByteArray(detail));
 
         return StakeModifyParam.builder()
                 .nodeId(nodeId)
-                .benefitAddress(address)
-                .externalId("0x".equals(externalId)?"":externalId)
-                .nodeName(nodeName)
-                .website(website)
-                .details(detail)
-                .delegateRewardPer(rewardPer.intValue()) // 非必填
+                .benefitAddress(benefitAddress) // 非必填
+                .externalId(externalId) // 非必填
+                .nodeName(nodeName) // 非必填
+                .website(website) // 非必填
+                .details(detail) // 非必填
+                .delegateRewardPer(rewardPer==null?null:rewardPer.intValue()) // 非必填
                 .build();
     }
 }
